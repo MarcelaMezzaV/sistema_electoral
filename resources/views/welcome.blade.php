662fb8d70386187221b5196a89a4c4a4ccbd0447
nacho <cruzi3836@gmail.com>
2020-06-05 14:46:29 -0400

    actualizando roles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Con la rama Jose</h1>
+                        <h1 class="m-0 text-dark">Sistema electoral</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
